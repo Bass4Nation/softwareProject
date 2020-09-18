5c7e10a35ba5c27fd73110c2b64ea3dbfbf3f051
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2020-09-18 16:19:23 +0200

    v001

--- parkering/src/main/java/no/hiof/Application.java
+++ parkering/src/main/java/no/hiof/Application.java
@@ -3,6 +3,6 @@ package no.hiof;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("HEI");
+        System.out.println("Dette er Application");
     }
 }
---
+++ parkering/src/main/java/no/hiof/model/Main.java
@@ -0,0 +1,8 @@
+package no.hiof.model;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Dette er Main i modell");
+    }
+}
