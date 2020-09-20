81a59992a5a2a0b098d9376d40e0b4632c5fd5ed
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2020-09-20 17:44:27 +0200

    Nettsiden til walter virker basic

--- parkering/src/main/java/no/hiof/model/Main.java
+++ parkering/src/main/java/no/hiof/model/Main.java
@@ -3,6 +3,11 @@ package no.hiof.model;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Dette er Main i modell");
+
+    ParkSpot park1 = new ParkSpot().spots(1,4,3, "Dette er en parkeringsplass");
+    ParkSpot park2 = new ParkSpot().spots(4,4,3, "Dette er en parkeringsplass");
+    ParkSpot park3 = new ParkSpot().spots(2,4,3, "Dette er en parkeringsplass");
+    ParkSpot park4 = new ParkSpot().spots(3,4,3, "Dette er en parkeringsplass");
+
     }
 }
