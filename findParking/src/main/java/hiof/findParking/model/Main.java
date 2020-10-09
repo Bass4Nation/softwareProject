e49f64f01036c7a295ccd3da1108d842c5283043
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2020-10-09 15:24:23 +0200

    Main og oppdatert zip
    fikset main. Var ikke helt riktig satt opp. Så nå trengs det bare for å starte det er å lage et startup application

--- findParking/src/main/java/hiof/findParking/model/Main.java
+++ findParking/src/main/java/hiof/findParking/model/Main.java
@@ -1,6 +1,16 @@
 package hiof.findParking.model;
 
 public class Main {
-//    Laget for å teste klasser som er i model.
+//    Laget for å teste hele prosjektet pluss nettsiden.
 
+
+    public static void main(String[] args) {
+        System.out.println("Dette er main i model");
+
+//        For loop som teller til hundre. For å sjekke om main virker
+        for(int i = 0; i <= 100; i++){
+            System.out.println(i);
+        }
+
+    }
 }
