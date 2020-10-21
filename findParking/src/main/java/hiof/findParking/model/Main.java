bf8e22ef9a1019274766f841996db62b75ab2056
waltereikrem <43746219+waltereikrem@users.noreply.github.com>
2020-10-21 18:27:39 +0200

    IGJEN fikset mer på struktur pga jeg gjorde en feil

--- findParking/src/main/java/hiof/findParking/model/Main.java
+++ findParking/src/main/java/hiof/findParking/model/Main.java
@@ -1,8 +1,6 @@
 package hiof.findParking.model;
 
 public class Main {
-    public static void main(String[] args) {
+//    Laget for å teste klasser som er i model.
 
-        //Laget for å teste klasser som er i model.
-    }
 }
